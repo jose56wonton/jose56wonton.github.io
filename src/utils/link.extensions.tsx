255d406d496ee881955456f12ebd8725307d6594
Joshua Wootonn <jose56wonton@gmail.com>
2020-04-04 17:58:48 -0500

    Move the footer.container.tsx and the about.container.tsx to the new Link

--- src/utils/link.extensions.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from '../components/typography';
-import React from 'react';
-
-export const mapLinkObjectToLinks = (object: { [key: string]: any }) =>
-    Object.keys(object).map((key: string) => (
-        <Link key={object[key]} href={object[key]}>
-            {key}
-        </Link>
-    ));
